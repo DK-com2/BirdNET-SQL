64746497eeb45d8470a36ffb0764edcbd91ca654
DKcom <daichispeak@gmail.com>
2025-08-21 22:37:27 +0900

    プログラムファイルの統合と削除

--- set_env.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set PROJECT_PATH=S:\python\BirdNet-win
-set SPECIES_FILE=S:\python\BirdNet-win\lib\species1.json
-set SPECIES_COLUMN=species1
-set PYTHONPATH=S:\python\BirdNet-win\lib;%PYTHONPATH%
-echo Environment variables set.
